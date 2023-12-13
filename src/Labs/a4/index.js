b2c8919a1048c5f89490c68b30439d1c6b416d1a
nicholasb4711 <nicholasb4711@gmail.com>
2023-12-12 20:10:01 -0500

    lab up to 2.3

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,9 +1,28 @@
-function Assignment4 () {
-    return (
-        <div>
-            <h1>Assignment 4</h1>
-        </div>
-    );
-}
+import React from "react";
+import Add from "./Add";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 
+function sayHello() {
+    alert("Hello");
+  }
+
+
+
+const Assignment4 = () => {
+ return(
+   <>
+     <h1>Assignment 4</h1>
+     <Add a={1} b={2} />
+     <ClickEvent />
+     <PassingDataOnEvent />
+     <PassingFunctions />       
+     <PassingFunctions theFunction={sayHello} />
+     <EventObject />
+ 
+   </>
+ );
+};
 export default Assignment4;
