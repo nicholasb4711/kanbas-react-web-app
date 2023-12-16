7297aabc70e78868f64a41a5d7094d872b409187
nicholasb4711 <nicholasb4711@gmail.com>
2023-12-15 22:51:20 -0500

    Update signin.js

--- src/Kanbas/users/signin.js
+++ src/Kanbas/users/signin.js
@@ -1,40 +1,20 @@
-import axios from "axios";
-
-const API_BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000/api";
-const USERS_API = `${API_BASE}/users`;
-
-export const signin = async (credentials) => {
-    const response = await axios.post(`${USERS_API}/signin`, credentials);
-    return response.data;
-}
-
-export const signup = async (credentials) => {
-    const response = await axios.post(
-      `${USERS_API}/signup`, credentials);
-    return response.data;
+import * as client from "./client";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+function Signin() {
+  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const navigate = useNavigate();
+  const signin = async () => {
+    await client.signin(credentials);
+    navigate("/Kanbas/Account");
   };
-
-export const account = async () => {
-    const response = await axios.post(`${USERS_API}/account`);
-    return response.data;
-};
-
-export const findAllUsers = async () => {
-    const response = await axios.get(USERS_API);
-    return response.data;
+  return (
+    <div>
+      <h1>Signin</h1>
+      <input value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})}/>
+      <input value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
+      <button onClick={signin}> Signin </button>
+    </div>
+  );
 }
-
-export const findUserById = async (id) => {
-    const response = await axios.get(`${USERS_API}/${id}`);
-    return response.data;
-}
-
-export const updateUser = async (id, user) => {
-    const response = await axios.put(`${USERS_API}/${id}`, user);
-    return response.data;
-}
-
-export const deleteUser = async (id) => {
-    const response = await axios.delete(`${USERS_API}/${id}`);
-    return response.data;
-}
+export default Signin;
